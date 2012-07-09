84347b58af79b32a828ab69f008a10af6b236493
AITI STUDENT <aiti-student@aiti-092-PC.(none)>
2012-07-09 15:36:59 +0000

    updates

--- blog/Untitled Folder/models.py
+++ blog/Untitled Folder/models.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.db import models
-
 class post(models.Model):
     title = models.CharField(max_length=60)
     body = models.TextField()
     created = models.DateField(auto_now = True)
     updated = models.DateField(auto_now = True)
-
     def __unicode__(self):
         return self.title
 
@@ -16,34 +14,30 @@ class comment(models.Model):
     created = models.DateField(auto_now = True)
     updated = models.DateField(auto_now = True)
     post = models.ForeignKey(post)
-
-    def __unicode__(self):
-        return self.author
-    
-
     def first_sixty(self):
         return self.body[:60]
     
-class CommentInline(admin.TabularInline):
-	model = comment
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'created', 'updated')
     search_fields = ('body', 'title')
-    list_filter = ('created', 'updated')
-    search_fields = ('title', 'body')
-    inlines = [CommentInline]
+    list_filter = ('created','updated')
+    search_fields = ('title','body')
     
 
 
 class CommentAdmin(admin.ModelAdmin):
 	list_display = ('post', 'author', 'first_sixty', 'created', 'updated')
 	list_filter = ('created', 'author' )
-	inlines = [CommentInline]
 
+
+#class CommentInline(admin.TabularInline):
+	#model = comment
 	
     
+#admin.site.register(post)
+#admin.site.register(comment)
 admin.site.register(post,PostAdmin)
 admin.site.register(comment,CommentAdmin) 
 
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,28 @@
 # Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from models import post, comment 
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    
+    print type(post_list)
+    print post_list
+    
+    return HttpResponse(post_list)
+
+def post_detail(request, id, showComments=False):
+    if showComments!=False:
+         current = {id:showComments} 
+    return HttpResponse(current[id])
+    
+def post_search(request, term):
+    result = re.search(r'.*', term)
+    return HttpResponse(result)
+    
+    
+
+def home(request):
+    print 'it works'
+    return HttpResponse('hello world. Ete zene?') 
